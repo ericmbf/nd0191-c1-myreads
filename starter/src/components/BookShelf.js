5f24acc77bd2c9f1295772b689fd6792265ae288
Eric Costa Hall <ericmbf@gmail.com>
2022-07-05 20:44:36 -0300

    Implement BookShelf component

---
+++ starter/src/components/BookShelf.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import BookList from "./BookList";
+
+const BookShelf = ({ books, title}) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <BookList books={books}/>
+            </div>
+        </div>
+    );
+}
+
+BookShelf.propTypes = {
+    books: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired,
+}
+
+export default BookShelf;
