fe903cd14a688fdcbb8fa5dbac1f3de0ed7ecfe4
Eric Costa Hall <ericmbf@gmail.com>
2022-07-05 20:44:12 -0300

    Implement MyBook component

--- starter/src/components/App.js
+++ starter/src/components/App.js
@@ -1,17 +1,24 @@
 import "../css/App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBooks from "./SearchBook"
 import MyBooks from "./MyBooks"
 import { Route, Routes } from 'react-router-dom';
+import * as BooksAPI from "../utils/BooksAPI"
 
 function App() {
+
+  const [allbooks, setAllBooks] = useState([]);
+  
+  useEffect(() => {
+    BooksAPI.getAll().then((res) => setAllBooks(res));
+  }, []);
 
   return (
     <Routes>
       <Route
         exact
         path="/"
-        element={<MyBooks />}
+        element={<MyBooks books={allbooks}/>}
       />
       <Route
         path="/search"
