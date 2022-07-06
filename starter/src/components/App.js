e498ee5a63ad58164e72eabc5d265143f051cb3f
Eric Costa Hall <ericmbf@gmail.com>
2022-07-06 12:45:04 -0300

    Implement search screen and query feature

--- starter/src/components/App.js
+++ starter/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
       />
       <Route
         path="/search"
-        element={<SearchBooks />} />
+        element={<SearchBooks books={allbooks}/>} />
     </Routes>
   );
 }
--- starter/src/components/SearchBook.js
+++ starter/src/components/SearchBook.js
@@ -1,34 +1,51 @@
 import PropTypes from "prop-types";
-import { useState } from 'react';
+import { useState} from 'react';
 import { Link } from "react-router-dom";
-import serializeForm from "form-serialize";
+import Book from "./Book";
 
-const SearchBook = ({ }) => {
+const SearchBook = ({ books }) => {
+
+    const [query, setQuery] = useState("");
+
+    const showingBooks =
+        query === "" ?
+            books : books.filter((c) =>
+                c.title.toLowerCase().includes(query.toLowerCase())
+            );
+
+    const handleQuery = (event) => {
+        setQuery(event.target.value);
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
-                <a
+                <Link
                     className="close-search"
-                // onClick={() => setShowSearchpage(!showSearchPage)}
-                >
-                    Close
-                </a>
+                    to="/"
+                />
                 <div className="search-books-input-wrapper">
                     <input
                         type="text"
                         placeholder="Search by title, author, or ISBN"
+                        value={query}
+                        onChange={(e) => handleQuery(e)}
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {showingBooks.map((book) => {
+                        return <Book book={book}/>
+                    })}
+                </ol>
             </div>
         </div>
     );
 }
 
 SearchBook.propTypes = {
-    // createContact: PropTypes.func.isRequired,
+    books: PropTypes.array.isRequired,
 }
 
 export default SearchBook;
